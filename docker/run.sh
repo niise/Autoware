f3e7fc88be4d6ed21dd4bde820640a4d4dca0d31
Niise Wu <niise.wu@acer.com>
2017-01-25 08:43:54 +0800

    modify the Dockerfile for cudnn

--- docker/run.sh
+++ docker/run.sh
@@ -3,11 +3,16 @@
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 
+# avoid /tmp/.docker.xauth already exist, please check the path
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
 nvidia-docker run \
         -it --rm \
         --volume=$XSOCK:$XSOCK:rw \
         --volume=$XAUTH:$XAUTH:rw \
-        --env="XAUTHORITY=${XAUTH}" \
+        --volume=/home/niise/.autoware:/home/autoware/.autoware:rw \
+	--env="XAUTHORITY=${XAUTH}" \
         --env="DISPLAY=${DISPLAY}" \
         -u autoware \
         autoware-image
